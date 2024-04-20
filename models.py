7ee2c3c2607183903fc7fc5e7222238ba0dc6385
Mr12 <221bst221@gmail.com>
2024-04-20 04:04:24 +0200

    create session fun, addPerson fun, fixed relactions after renameing

--- models.py
+++ models.py
@@ -5,48 +5,99 @@ from datetime import datetime
 
 
 
+def createSession():
+    try:
+        engine = create_engine('sqlite:///E.db')
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        return session
+    except:
+        return False
+
 Base = declarative_base()
 
-class Person(Base):
+class Persondb(Base):
     __tablename__ = 'person'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
-    phoneNumber = Column(String(15))
+    phoneNumber = Column(String(15), nullable=False, unique=True, index=True )
     birthday = Column(String(10))
     country = Column(String(50))
     address = Column(String(100))
     creationTime    = Column(DateTime , default = datetime.now())
 
     # Define one-to-many relationship with WhatsApp table
-    whastappEntries = relationship('whatsApp', back_populates='person')
-    TwitterEntries = relationship('Twitter', back_populates='person')
+    whatsappEntries = relationship('whatsAppdb', back_populates='person')
+    TwitterEntries = relationship('Twitterdb', back_populates='person')
 
-class whatsApp(Base):
+
+    # functions stuff
+    def addPerson(self, session):
+        """
+        Add the current instance to the database.
+
+        """
+        try:
+            existingUser = session.query(Persondb).filter_by(phoneNumber=self.phoneNumber).first()
+            if existingUser:
+                print("user Found")
+                return False
+            else:
+                session.add(self)
+                return True
+        except:
+            return False
+        
+    def searchForPerson(self, session):
+        """
+        Search for a person by their phone number.
+
+        Args:
+            session
+
+        Returns:
+            Persondb or None: The person object if found, or None if not found.
+        """
+        try:
+            # Query the database for a person with the given phone number
+            person = session.query(Persondb).filter_by(phoneNumber=self.phoneNumber).first()
+            return person
+        except:
+            return False
+
+class whatsAppdb(Base):
     __tablename__ = 'whatsApp'
     id = Column(Integer, primary_key=True)
-    CurrentProfilePic = Column(String(100))
-    CurrentAboutStatus = Column(String(200))
-    phoneNumber = Column(String(15), nullable=False)
+    currentProfilePic = Column(String(100))
+    currentAboutStatus = Column(String(140))
+    currentbussinessCover = Column(String(100))
+    currentbussinessName = Column(String(50))
+
+    lastOnline = Column(DateTime, nullable=True)
+
+
+    phoneNumber = Column(String(17), nullable=False)
 
     # Define the foreign key relationship
     personId = Column(Integer, ForeignKey('person.id'))
-    person = relationship('Person', back_populates='whastappEntries')
+    person = relationship('Persondb', back_populates='whatsappEntries')
     # One-to-Many relationship with ProfilePic
     profilePics = relationship('ProfilePic', back_populates='whatsapp', cascade='all, delete-orphan')
 
 
 
-class Twitter(Base):
+class Twitterdb(Base):
     __tablename__ = 'Twitter'
     id = Column(Integer, primary_key=True)
-    CurrentProfilePic = Column(String(100))
-    CurrentBio = Column(String(200))
+    currentProfilePic = Column(String(100))
+    currentBio = Column(String(200))
     userName = Column(String(15), nullable=False)
     profilePics = relationship('ProfilePic', back_populates='twitter', cascade='all, delete-orphan')
 
     # Define the foreign key relationship
     personId = Column(Integer, ForeignKey('person.id'))
-    person = relationship('Person', back_populates='TwitterEntries')
+    person = relationship('Persondb', back_populates='TwitterEntries')
 
 
 
@@ -58,12 +109,12 @@ class ProfilePic(Base):
     path = Column(String(100))
     #entity_type = Column(String(50))  # Type of the associated entity (e.g., WhatsApp, Twitter, Facebook)
     #entity_id = Column(Integer)        # ID of the associated entity
-    created_at = Column(String(20), default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    createdAt = Column(DateTime, default=datetime.now)
     hash = Column(String(32))
     
     whatsappId = Column(Integer, ForeignKey('whatsApp.id'))
-    whatsapp = relationship('whatsApp', back_populates='profilePics')
+    whatsapp = relationship('whatsAppdb', back_populates='profilePics')
     
     TwitterId = Column(Integer, ForeignKey('Twitter.id'))
-    twitter = relationship('Twitter', back_populates='profilePics')
+    twitter = relationship('Twitterdb', back_populates='profilePics')
     
