c60f61c2d1700fc1380e950257ebf003957e576a
Mr12 <221bst221@gmail.com>
2024-04-27 08:01:35 +0300

    add username, and persondb to handle database from the person class

--- Person.py
+++ Person.py
@@ -1,10 +1,14 @@
 
+from models import Persondb
+
 class Person:
-    def __init__(self, name=None, dateOfBirth=None, phoneNumber=None, nickName=None):
+    def __init__(self, name=None, dateOfBirth=None, phoneNumber=None, nickName=None, username=None):
         self.name = name
         self.dateOfBirth = dateOfBirth
         self.phoneNumber = phoneNumber
         self.nickName = nickName
+        self.username = username
+        self.persondb = Persondb(name=name, username=username)
     
     def calculateAge(self):
         pass
